61c905ab740b8113efe5dfd2b2e267cff47197ac
Connor <cmurphy227@west-mec.org>
2020-03-12 17:44:22 -0700

    Finished case 2

---
+++ case2/mt_calc.js
@@ -0,0 +1,113 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 11
+   Case Problem 2
+
+   Author: Connor Murphy
+   Date: 3/12/20  
+   
+   Filename: mt_calc.js
+	
+   Functions List:
+
+   init()
+      Initializes the contents of the web page and sets up the
+      event handlers
+      
+   buttonClick(e)
+      Adds functions to the buttons clicked within the calcutlor
+      
+   calcKeys(e)
+      Adds functions to key pressed within the calculator window 
+      
+   eraseChar(textStr)
+      Erases the last character from the text string, textStr
+      
+   evalEq(textStr, decimals) 
+      Evaluates the equation in textStr, returning a value to the number of decimals specified by the decimals parameter
+
+   lastEq(textStr) 
+      Returns the previous expression from the list of expressions in the textStr parameter
+
+*/
+
+window.onload = init;
+
+function init(){
+   //allows buttons/keys to work when clicked
+   var calcButtons = document.getElementsByClassName("calcButton");
+   for(var i = 0; i < calcButtons.length; i++){
+      calcButtons[i].addEventListener("click", buttonClick);
+   }
+   document.getElementById("calcWindow").addEventListener("keydown", calcKeys);
+}
+function buttonClick(e){
+   var calcValue = document.getElementById("calcWindow").value;
+   var calcDecimal = document.getElementById("decimals").value;
+   var buttonValue = e.target.value;
+   switch(buttonValue){
+      //resets the calculator
+      case "del":
+         calcValue = "";
+         break;
+      //erases last character
+      case "bksp":
+         calcValue = eraseChar(calcValue);
+         break;
+      //solves the equation
+      case "enter":
+         calcValue += " = " + evalEq(calcValue, calcDecimal) + "\n";
+         break;
+      //returns to previously entered equation
+      case "prev":
+         calcValue = lastEq(calcValue);
+         break;
+      default:
+         calcValue = calcValue + buttonValue;
+         break;
+   }
+   document.getElementById("calcWindow").value = calcValue;
+   document.getElementById("calcWindow").focus();
+}
+function calcKeys(e){
+   var calcValue = document.getElementById("calcWindow").value;
+   var calcDecimal = document.getElementById("decimals").value;
+   switch(e){
+      //resets the calculator
+      case "Delete":
+         calcValue = "";
+         break;
+      //solves the equation
+      case "Enter":
+         calcValue += " = " + evalEq(calcValue, calcDecimal);
+         break;
+      //returns to previously entered equation
+      case "ArrowUp":
+         calcValue += lastEq(calcWindow.value);
+         e.preventDefault();
+         break;
+   }
+   document.getElementById("calcWindow").value = calcValue;
+}
+
+
+/* ===================================================================== */
+
+function eraseChar(textStr) { 
+   return textStr.substr(0, textStr.length - 1);
+}
+
+function evalEq(textStr, decimals) {
+   var lines = textStr.split(/\r?\n/);
+   var lastLine = lines[lines.length-1];
+   var eqValue = eval(lastLine);
+   return eqValue.toFixed(decimals);
+}  
+
+function lastEq(textStr) {
+   var lines = textStr.split(/\r?\n/);
+   var lastExp = lines[lines.length-2];
+   return lastExp.substr(0, lastExp.indexOf("=")).trim();
+}
